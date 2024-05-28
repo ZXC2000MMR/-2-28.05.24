870d58c6de9e08673925031a9a8dd059289e112c
Cybersport <Cybersport@26kadr.internal>
2024-05-28 13:04:52 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,152 @@
+
+1
+//alert('Я JavaScript!')
+
+
+
+3
+//let name = "Ilya";
+//alert( `hello ${1}` ); // ?
+//alert( `hello ${"Илюша свинья"}` ); // ?
+//alert( `hello ${name}` ); // ?
+
+
+
+4
+//grop = prompt('Введите имя' ,);
+//alert(`Добро пожаловать на сайт ${grop}`)
+
+2
+//name = "Иван"
+//admin = name
+//alert(admin)
+
+
+5
+// let a = 1, b = 1;
+// let c = ++a; // ?
+// let d = b++; // ?
+// alert(a)
+// alert(b)
+// alert(c)
+// alert(d)
+
+
+6
+// let a = 2;
+// let x = 1 + (a *= 2);
+// alert(x)
+// alert(a)
+
+
+
+7
+// let a = prompt("Первое число?", 1);
+// let b = prompt("Второе число?", 2);
+// a =  Number (a)
+// b =  Number (b)
+// alert (Number(a + b)); //12
+
+
+
+8
+// 7 > 5
+// "стол" > "стул"
+// "4" > "52"
+// undefined == null
+// undefined === null
+// null == "\n0\n"
+// alert(null === +"\n0\n")
+
+9
+// if ("0") {
+//     alert( 'Привет' );
+//   }
+10
+//  result = prompt("Какое «официальное» название JavaScript?")
+// if (result == 'ECMAScript') {
+//    alert('Верно!');
+// } 
+//  else {
+//     alert( 'Не знаете? ECMAScript!');
+// }
+
+11
+// a = prompt("введите число")
+// if(a > 0)
+//     {
+//     alert('1');
+//     }
+// else if (a<0)
+//     {
+//     alert("-1");
+//     }
+// else if (a == 0)
+//     {
+//     alert('0');
+//     }
+
+12
+// let result;
+// a =Number(prompt())
+// b =Number(prompt())
+// result = (a + b < 4) ? alert('мало'): alert('много');
+ 13
+//  login = prompt('введите логин')
+//  let message;
+//  message = (login == 'Сотрудник')?  alert('Привет'):
+//  message = (login == 'Директор')? alert('Здравствуйте'):
+//  message = (login == "")? alert('Нет логина'): ''
+ 
+14
+// alert( null || 4 || undefined );
+// выводит 4
+15
+// alert( alert(1) || 2 || alert(3) );
+// выводит 12
+16
+//  alert( 1 && null && 2 );
+// выводит null
+17
+//  alert( alert(1) && alert(2) );
+//выводит undefined
+18
+//  alert( null || 2 && 3 || 4 );
+//выводит 3
+19
+//  let value = NaN
+//  value &&= 10;
+//  value ||= 20;
+//  value &&= 30;
+//  value ||= 40;
+
+//  alert(value);
+//выводит 30
+20
+// let age = prompt('введите возраст')
+// let message
+// message = (age >= 14 && age <= 90)? alert("Доступ Есть") : alert("Доступа нет")
+21
+// let age = prompt('введите возраст')
+// let message
+// message = (age >= 17 && age <= 99)? alert(" Доступа нет") : alert("Доступ Есть")
+22
+// if (-1 || 0) alert( 'first' );
+// if (-1 && 0) alert( 'second' );
+// if (null || -1 && 1) alert( 'third' );
+23
+// let login = prompt ("ВВедите логин")
+// let pasword
+// if(login = "Админ")
+// {
+//     prompt("Пароль?")
+// }
+// else if (login != "Админ") 
+// {
+//     alert("Я вас незнаю")
+// }
+// if (pasword = "Я главный"){alert("Здравствуйте")}
+// else if (pasword = "Я главный") {alert("неверный пароль")}
+
+
+
