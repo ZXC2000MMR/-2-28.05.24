1d5cac56650ac28bf22df7c38c31352112dad3ac
Cybersport <Cybersport@26kadr.internal>
2024-05-30 14:56:31 +0300

    zadanie 29

--- app.js
+++ app.js
@@ -21,14 +21,53 @@ list.appendChild(ol)
 }
 function addilist()
 {
-let add = document.getElementById("deldiv")
-var div = document.createElement("div")
-div.innerHTML = ("chicken");
-
+let del = document.getElementById("deldiv")
+del.innerHTML = ('')
 }
-function dellist()
+function izmenphoto()
 {
-    let del = document.getElementById("deldiv")
-     del.innerHTML = ('')
+   let click = document.getElementById("photoizmen")
+   document.getElementById('photo').src = 'photografi/photo2.jpg';
 }
+function nazad()
+{
+   let click = document.getElementById("photonazad")
+   document.getElementById('photo').src = 'photografi/photo.jpg';
+}
+function knopkacvet()
+{
+    
+    document.getElementById("cvetknopki").classList.toggle("blue");
+    document.getElementById("cvetknopki").classList.toggle("red");
 
+}
+function knopkabob()
+{
+    let text = document.getElementById("bob")
+    text.setAttribute("class","bob1")
+    text.innerHTML=("Губка боб")
+}
+function knopkabobdel ()
+{
+let remove = document.getElementById("bob")
+remove.removeAttribute("class")
+remove.innerHTML=("Кто проживает на дне океана?")
+}
+document.addEventListener("click", function(e) {
+    let m = document.getElementById('menu');
+    if (e.target.id != 'test' && e.target.id != 'menu') {
+      m.style.display = 'none';
+    } else if (e.target.id == 'test') {
+      m.style.display = (m.style.display != 'block') ? 'block' : 'none';
+    }
+  });
+
+  let input = document.querySelector('input'),
+    btn = document.querySelector('button'),
+    txt = document.querySelector('.txt');
+btn.addEventListener('click', () => {
+    txt.textContent += input.value;
+})
+const btn1 = document.body.appendChild(document.createElement('button'));
+ 
+btn1.onclick = (v => (btn1.innerText = v, _ => btn1.innerText = ++v))(1);
